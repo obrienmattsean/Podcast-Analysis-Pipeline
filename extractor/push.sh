b3028c079160bc3245d9c38cca9915625bbd4e52
alex <alexguo2514@gmail.com>
2026-05-29 13:25:34 +0100

    removed utility bash file

--- extractor/push.sh
+++
@@ -1,7 +0,0 @@
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
-
-docker build -t c23-podex-ai-extract --platform="linux/amd64" --provenance=false .
-
-docker tag c23-podex-ai-extract:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c23-podex-ai-extract:latest
-
-docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c23-podex-ai-extract:latest
