4200428c06456eaf035f8ebbff80b6150872e7a2
Danny Bozbay <107803920+dbozbay@users.noreply.github.com>
2026-06-03 15:41:32 +0100

    fix: correct formatting of return button in podcast details page

--- dashboard/pages/podcast_details.py
+++ dashboard/pages/podcast_details.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 return_button = st.page_link(
-    "pages/1_Library.py", label="Return to Library", use_container_width=True)
+    "pages/1_Library.py", label="Return to Library", use_container_width=True
+)
 
 podcast_title = st.query_params.get("podcast_title")
 st.header(f"Analytics for {podcast_title}.")
